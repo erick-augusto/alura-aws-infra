7b7f4de3abf3a863d9f0bf5f50970c5251358a1a
erick-augusto <erick_aug@hotmail.com>
2026-02-26 10:10:17 -0300

    Criando o VPC

--- src/main/java/com/myorg/AluraVPCStack.java
+++ src/main/java/com/myorg/AluraVPCStack.java
@@ -5,7 +5,7 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
-
+import software.amazon.awscdk.services.ec2.Vpc;
 public class AluraVPCStack extends Stack {
     public AluraVPCStack(final Construct scope, final String id) {
         this(scope, id, null);
@@ -14,11 +14,8 @@ public class AluraVPCStack extends Stack {
     public AluraVPCStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AluraAwsInfraQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Vpc vpc = Vpc.Builder.create(this, "AluraVpc")
+                    .maxAzs(3)  // Default is all AZs in region
+                    .build();
     }
 }
