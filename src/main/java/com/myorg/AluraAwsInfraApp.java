34b6a7ebb35b2850c178a202c30fef3514393c01
erick-augusto <erick_aug@hotmail.com>
2026-02-27 10:18:38 -0300

    Estrutura para criar o banco de dados

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -9,11 +9,13 @@ public class AluraAwsInfraApp {
         AluraVPCStack vpcStack = new AluraVPCStack(app, "Vpc");
         AluraClusterStack clusterStack = new AluraClusterStack(app, "Cluster", vpcStack.getVpc());
         clusterStack.addDependency(vpcStack);
+
+        AluraRdsStack rdsStack = new AluraRdsStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
+
         AluraServiceStack aluraServiceStack = new AluraServiceStack(app, "Service", clusterStack.getCluster());
         aluraServiceStack.addDependency(clusterStack);
-        app.synth();
-
+        aluraServiceStack.addDependency(rdsStack);
         app.synth();
     }
 }
-
